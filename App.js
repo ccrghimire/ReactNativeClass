7631f46ac5afda9feb11642b15b4ea1d8f0491ab
Shisir <ghimireccr@gmail.com>
2021-02-10 23:34:44 +0545

    simple card completed

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {View, Text, StyleSheet,} from 'react-native';
 
-export default function App() {
+
+    const App = () => {
+  const data = ['Sneha', 'Sunisha', 'Rojina'];
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <View style={styles.screen}>
+            <Heading>John Smith</Heading>
+      {
+        data.map(a=>{
+            return(
+              <View key={a} style={styles.card}>
+              <Text style={{ color: 'white' }}> <Text style={{fontWeight:'bold'}}>{a}</Text> likes your photo</Text>
+            </View>
+            )
+      
+          
+          })
+
+      }
+      <Title>Flutter </Title>
+      <Text>Description</Text>
+
+    </View >
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+screen:{
+  flex:1,
+  backgroundColor:'pink',
+  marginTop: 30,
+  padding:20
+},
+title:{
+   fontSize:20,
+  fontWeight:'bold'
+},
+card:{
+  width:'100%',
+  backgroundColor:'grey',
+  marginTop:20,
+  padding:10
+}
+})
+export default App;
