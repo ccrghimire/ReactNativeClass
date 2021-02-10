f7ce72674cf2863968604b6ba9c1343d90fb78af
Shisir <ghimireccr@gmail.com>
2021-02-10 23:49:35 +0545

    project one completed

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, View, Text,} from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.card}>
+        <Text style={{ fontSize: 40, color: '#444' }}>JS</Text>
+      </View>
+      <Text style={{ fontSize: 30, color: '#D56EFE', marginTop: 30, }}>John Smiths</Text>
+      <Text>UI/UX Designer</Text>
+      <Text style={{ marginTop: 30, }}>Basically, no, they define different types of activities. Copywriters’ main goal is creating texts that sell something, be it products, services, tools, intellectual and creative stuff, etc. UX writers’ task is creating texts that support and enhance users communication with interfaces, let them clearly understand what’s going on and lighten the negative experience in case of errors or problems of interaction.</Text>
+      <View style={styles.cards}>
+        <Text style={{ fontSize: 40, color: '#522E76',textAlign:20, }}>250 </Text>
+      </View>
+    
     </View>
-  );
+
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    marginTop: 50,
+    padding: 30,
   },
-});
+  card: {
+    width: '20%',
+    height: 75,
+    borderRadius: 100,
+    marginTop: 40,
+    borderColor: '#CE69FF',
+    alignItems: 'center',
+    borderWidth: 5,
+  },
+  cards:{
+    width:'20%',
+    fontSize:24,
+    marginTop:120,
+    borderRadius:5,
+
+
+
+  }
+})
+
+export default App;
