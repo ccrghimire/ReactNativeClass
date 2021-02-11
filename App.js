c1e457ec2d54c1bd58d3454eff3494a76b33944f
Shisir <ghimireccr@gmail.com>
2021-02-11 16:57:24 +0545

    calculator keyboard added

--- App.js
+++ App.js
@@ -1,21 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+      <View style={styles.view1}>
+        <Text style={styles.test}>Calculator display </Text>
+      </View>
+            <View style={styles.view2}>
+        <View style={styles.footerContainer}>
+          <View>
+            <Text style={styles.test}>1</Text>
+          </View>
+          <View>
+            <Text style={styles.test}>2</Text>
+          </View>
+          <View>
+            <Text style={styles.test}>3</Text>
+          </View>
+          <View>
+            <Text style={styles.test}>#</Text>
+          </View>
+        </View>
+        <View style={styles.footerContainer}>
+          <View>
+            <Text style={styles.test}>4</Text>
+          </View>
+          <View >
+            <Text style={styles.test}>5</Text>
+          </View>
+          <View>
+            <Text style={styles.test}>6</Text>
+          </View>
+          <View>
+            <Text style={styles.test}>=</Text>
+          </View>
+        </View>
+        <View style={styles.footerContainer}>
+          <View>
+            <Text style={styles.test}>7</Text>
+          </View>
+          <View>
+            <Text style={styles.test}>8</Text>
+          </View>
+          <View>
+            <Text style={styles.test}>9</Text>
+          </View>
+          <View>
+            <Text style={styles.test}>.</Text>
+          </View>
+          </View>
+      </View>
+      </View>
+    )};
+
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection:'column',
+          
   },
+
+  view1:{
+    flex:2,
+    flexDirection:'row',
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'white',
+    
+  },
+   view2:{
+    flex:3,
+    backgroundColor:'grey',
+    justifyContent:'space-around',
+    
+  },
+
+  test:{
+  fontSize:34,
+  fontWeight:'bold',
+  color:"black",
+   
+  },
+  footerContainer:{
+    flexDirection:'row',
+    justifyContent:'space-around',
+  }
 });
