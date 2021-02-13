416f8d2137dffb01e2405e3bdbbd71095a6b9338
Shisir <ghimireccr@gmail.com>
2021-02-13 16:29:28 +0545

    button and custombutton created

--- App.js
+++ App.js
@@ -1,12 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button,ActivityIndicator, Image } from 'react-native';
+import {CustomButton, CustomButton2, CustomButton3, CustomButton4} from './components/buttons';
 
 export default function App() {
+const clickHandler =()=>{
+  console.log('Button clicked')
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Buttons</Text>
+      <View style={{ width: "80%" }}>
+        <Button title="click me" onpress={clickHandler} />
+      </View>
+      <View style={{ width: "80%" }}>
+        <Button title="click me" color="red" onpress={clickHandler} />
+      </View>
+      <View style={{ width: "80%", marginBottom: 40 }}>
+        <Button title="click me" color="blue" disabled onpress={clickHandler} />
+      </View>
+      <CustomButton
+        color="red"
+        textColor="white"
+        title="TouchableOpacity"
+        onButtonPlaceCustomEventHandler={clickHandler}
+      />
+      <Text>Custom Button</Text>
+      <CustomButton2
+        color="blue"
+        textColor="white"
+        title="TouchableHighlight"
+        onButtonPlaceCustomEventHandler={clickHandler}
+      />
+      <Text>Custom Button</Text>
+      <CustomButton3
+        color="red"
+        textColor="white"
+        title="TouchableHighlight"
+        onButtonPlaceCustomEventHandler={clickHandler}
+      />
+      <Text>Custom Button</Text>
+      <CustomButton4
+        color="red"
+        textColor="white"
+        title="TouchableHighlight"
+        onButtonPlaceCustomEventHandler={clickHandler}
+      />
+      <Text>Custom Button</Text>
+      <ActivityIndicator color="red" size="large" />
+      <Image style={styles.image} source={{uri:"https://storage.pixteller.com/designs/designs-images/2016-11-19/02/thumbs/img_page_1_58305b35ebf5e.png"}}/>
     </View>
   );
 }
@@ -14,8 +56,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'pink',
+    alignItems: "center",
   },
+    Image:{
+    width: 100,
+    height: 100,
+    borderRadius:50,
+    resizeMode:'cover'
+    }
 });
---
+++ components/buttons/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {View, Text, StyleSheet,  TouchableOpacity, TouchableHighlight, TouchableNativeFeedback, TouchableWithoutFeedback} from 'react-native';
+
+const CustomButton =props=>{
+    return (
+    <TouchableOpacity activeOpacity={0.5} onPress={props.onButtonPlaceCustomEventHandler}>    
+    <View styles={{...styles.button, backgroundColor:props.color}}>
+        <Text style={{...styles.buttonText,color:props.textColor}}>{props.title}</Text>
+    </View>
+    </TouchableOpacity>
+    )
+}
+const CustomButton2 =props=>{
+    return (
+    <TouchableHighlight onPress={props.onButtonPlaceCustomEventHandler}>    
+    <View styles={{...styles.button, backgroundColor:props.color}}>
+        <Text style={{...styles.buttonText,color:props.textColor}}>{props.title}</Text>
+    </View>
+    </TouchableHighlight>
+    )
+}
+const CustomButton3 =props=>{
+    return (
+    <TouchableNativeFeedback onPress={props.onButtonPlaceCustomEventHandler}>    
+    <View styles={{...styles.button, backgroundColor:props.color}}>
+        <Text style={{...styles.buttonText,color:props.textColor}}>{props.title}</Text>
+    </View>
+    </TouchableNativeFeedback>
+    )
+}
+const CustomButton4 =props=>{
+    return (
+    <TouchableWithoutFeedback onPress={props.onButtonPlaceCustomEventHandler}>    
+    <View styles={{...styles.button, backgroundColor:props.color}}>
+        <Text style={{...styles.buttonText,color:props.textColor}}>{props.title}</Text>
+    </View>
+    </TouchableWithoutFeedback>
+    
+    )
+}
+const styles = StyleSheet.create({
+    button:{
+        alignItems:"center",
+        justifyContent:"center",
+        padding:30,
+        padding:8,
+        borderRadius:5,
+    },
+    buttonText:{
+        fontWeight:'bold',
+    }
+})
+
+export  {CustomButton, CustomButton2, CustomButton3, CustomButton4};
